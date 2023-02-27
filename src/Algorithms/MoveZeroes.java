a45048c82877931725c9255ee835c690f4a41645
CHAN W1LL <303661031@qq.com>
2023-02-27 12:24:46 +0800

    121

--- src/Algorithms/MoveZeroes.java
+++ src/Algorithms/MoveZeroes.java
@@ -45,4 +45,19 @@ public class MoveZeroes {
         nums[right] = temp;
     }
 
+    public void moveZeroes03(int[] nums) {
+        if(nums==null){
+            return;
+        }
+        int j = 0;
+        for(int i = 0;i<nums.length;++i){
+            if(nums[i]!=0){
+                nums[j++] = nums[i];
+            }
+        }
+        for(int i=j;i<nums.length;++i){
+            nums[i] = 0;
+        }
+    }
+
 }
