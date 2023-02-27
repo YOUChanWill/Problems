ec0df07e86a90261ee11e5b058bf36202e5ee93d
CHAN W1LL <303661031@qq.com>
2023-02-27 12:25:57 +0800

    121

--- src/DataStructures/MaxProfit.java
+++ src/DataStructures/MaxProfit.java
@@ -7,9 +7,9 @@ public class MaxProfit {
         int maxprofit = 0;
         for (int i = 0; i < prices.length; i++) {
             if (prices[i] < minprice){
-                minprice = prices[i];
+                minprice = prices[i]; // 记录最低价格
             } else if (prices[i] - minprice > maxprofit) {
-                maxprofit = prices[i] - minprice;
+                maxprofit = prices[i] - minprice;  // 当前价格减去最低价格获取的利润最大
             }
         }
         return maxprofit;
