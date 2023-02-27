0282940b5ecff6d57b71e261164452f404c023be
CHAN W1LL <303661031@qq.com>
2023-02-27 13:30:15 +0800

    11

---
+++ src/Day/MaxArea.java
@@ -0,0 +1,22 @@
+package Day;
+
+public class MaxArea {
+    public static void main(String[] args) {
+
+    }
+    public int maxArea(int[] height) {
+        int n = height.length;
+        int left = 0, right = n - 1, ans = 0;
+        while (left < right){
+            int area = Math.min(height[left], height[right]) * (right - left);// 数组中的短板来决定最终的长度
+            ans = Math.max(ans, area);
+            if (height[left] <= height[right]) {
+                ++left;
+            }
+            else {
+                --right;
+            }
+        }
+        return ans;
+    }
+}
