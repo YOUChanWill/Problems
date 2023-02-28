5bbb3cb6b5e7da6f6ad123d94ce289475220fec3
CHAN W1LL <303661031@qq.com>
2023-02-28 19:32:12 +0800

    杨辉三角

--- src/DataStructures/Generate.java
+++ src/DataStructures/Generate.java
@@ -1,25 +1,22 @@
-//package DataStructures;
-//
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.List;
-//
-//public class Generate {
-//
-//    public List<List<Integer>> generate(int numRows) {
-//        List<List<Integer>> list = new ArrayList<>();
-//
-//        if (numRows == 1) {
-//            return list;
-//        }
-//
-//        for (int i = 1; i <= numRows; i++) {
-//            int[] ints = new int[i];
-//            ints[i-1] = 1;
-//            for (int j = 1 ; j <= numRows; j++){
-//                int[j] =
-//            }
-//        }
-//        return list;
-//    }
-//}
+package DataStructures;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Generate {
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ret = new ArrayList<>();
+        for (int i = 0; i <= numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0 ; j <= i; j++){
+                if (j == 1 || j == 0){
+                    row.add(1);
+                }else {  row.add(ret.get(i - 1).get(j - 1) + ret.get(i - 1).get(j));}
+            }
+            ret.add(row);
+        }
+        return ret;
+    }
+}
