50cdb64902bb23f90c2888436aba7e9a06aedeee
CHAN W1LL <303661031@qq.com>
2023-03-01 20:15:49 +0800

    选择排序

--- src/Study/SelectionSort.java
+++ src/Study/SelectionSort.java
@@ -1,7 +1,41 @@
 package Study;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SelectionSort {
 
+    public static void main(String[] args) {
+        SelectionSort sort = new SelectionSort();
+        int[] list = {1,2,6,3,2,9,4};
+        sort.selectionSort(list);
+        for (int nums :
+                list) {
+            System.out.print(nums);
+        }
+    }
+
+    public int selectionSmallset(int[] list){
+        int smallest = list[0];
+        int smallest_index = 0;
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] < smallest){
+                smallest = list[i];
+                smallest_index = i;
+            }
+        }
+        return smallest_index;
+    }
+
+    public int[] selectionSort(int[] list){
+        int[] ints = new int[list.length];
+        int smallest;
+        for (int i = 0; i < list.length; i++) {
+            smallest = selectionSmallset(list);
+            ints[i] = smallest;
+        }
+        return ints;
+    }
 
 
 }
