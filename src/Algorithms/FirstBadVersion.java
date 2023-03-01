37fd0a23b49a0941b136c3256862ff1809826906
CHAN W1LL <303661031@qq.com>
2023-03-01 18:08:34 +0800

    二分查找：版本回溯

--- src/Algorithms/FirstBadVersion.java
+++ src/Algorithms/FirstBadVersion.java
@@ -1,27 +1,34 @@
-//package Algorithms;
-//
-//public class FirstBadVersion {
-//    public static void main(String[] args) {
-//
-//    }
-//
-//
-//// 假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
-//// 你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version 是否在单元测试中出错。
-//// 实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。
-//
-//    public int firstBadVersion(int n) {
-//        int first = 1,end = n;
-//        while (first < end) { // 循环直至区间左右端点相同
-//            int mid = first + (end - first) / 2; // 防止计算时溢出
-//            if (isBadVersion(mid)) {
-//                end = mid; // 答案在区间 [left, mid] 中
-//            } else {
-//                first = mid + 1; // 答案在区间 [mid+1, right] 中
-//            }
-//        }
-//        // 此时有 left == right，区间缩为一个点，即为答案
-//        return first;
-//    }
-//}
-//
+package Algorithms;
+
+public class FirstBadVersion {
+    public static void main(String[] args) {
+
+    }
+
+
+// 假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
+// 你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version 是否在单元测试中出错。
+// 实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。
+
+    public int firstBadVersion(int n) {
+        int first = 1,end = n;
+        while (first < end) { // 循环直至区间左右端点相同
+            int mid = first + (end - first) / 2; // 防止计算时溢出
+            if (isBadVersion(mid)) {
+                end = mid; // 答案在区间 [left, mid] 中
+            } else {
+                first = mid + 1; // 答案在区间 [mid+1, right] 中
+            }
+        }
+        // 此时有 left == right，区间缩为一个点，即为答案
+        return first;
+    }
+
+    public boolean isBadVersion(int version){
+        return true;
+    }
+
+
+
+}
+
