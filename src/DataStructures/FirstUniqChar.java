43c61c5b2f0dd8620a061fc7e22aa5946ee418b3
CHAN W1LL <303661031@qq.com>
2023-03-02 14:11:45 +0800

    字符串：字符串中第一个不重复的元素

--- src/DataStructures/FirstUniqChar.java
+++ src/DataStructures/FirstUniqChar.java
@@ -1,5 +1,6 @@
 package DataStructures;
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class FirstUniqChar {
@@ -7,12 +8,21 @@ public class FirstUniqChar {
 //  给定一个字符串 s ，找到它的第一个不重复的字符，并返回它的索引 。如果不存在，则返回 -1
 
     public int firstUniqChar(String s) {
-        HashSet hashSet = new HashSet();
-        int index = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (!hashSet.add(s.charAt(i))) {
-                index = i;
+        int n = s.length();
+        int[] index = new int[26]; // 记录26个字母的出现的次数的数组
+        Arrays.fill(index,-1); // 使用-1填充数组
+        for (int i = 0; i < n; i++) {
+            int c = s.charAt(i) - 'a';
+            if (index[c] != -2){
+                index[c] = index[c] == -1 ? i : -2;
             }
         }
+        int res = n;
+        for (int i = 0; i < 26; i++) {
+            if (index[i] != -1 && index[i] != -2){
+                res = Math.min(res,index[i]);
+            }
+        }
+        return res == n ? -1 : res;
     }
 }
