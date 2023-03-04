4fedaab4d8c0c5b59b1357adfaeb1573136b7e72
CHAN W1LL <303661031@qq.com>
2023-03-04 23:46:20 +0800

    二分查找

---
+++ BinarySearch/base/Search.java
@@ -0,0 +1,20 @@
+package base;
+
+public class Search {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int mid = (left + right) / 2;
+
+        while (left<=right) {
+            if (target > nums[mid]) {
+                left = mid + 1;
+            } else if (target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+            mid = (left + right) / 2;
+        }
+        return -1;
+    }
+}
