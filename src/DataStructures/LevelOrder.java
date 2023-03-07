1c1ff72d0a03cb637a01b8adfae25bb2a7493e88
CHAN W1LL <303661031@qq.com>
2023-03-07 20:22:48 +0800

    二叉树的层序遍历

---
+++ src/DataStructures/LevelOrder.java
@@ -0,0 +1,45 @@
+package DataStructures;
+
+import java.util.*;
+
+public class LevelOrder {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        if (root != null){
+            queue.add(root);
+        }
+        while (!queue.isEmpty()){
+            int n = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null){
+                    queue.add(node.left);
+                }
+                if (node.right != null){
+                    queue.add(node.right);
+                }
+            }
+            res.add(level);
+        }
+        return res;
+    }
+}
+
+
+class TreeNode {
+int val;
+TreeNode left;
+TreeNode right;
+TreeNode() {}
+TreeNode(int val) { this.val = val; }
+TreeNode(int val, TreeNode left, TreeNode right) {
+this.val = val;
+this.left = left;
+this.right = right;
+}
+}
