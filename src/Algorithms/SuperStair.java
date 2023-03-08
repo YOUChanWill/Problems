bfc518de39f8328d7ffc55256904213c7a2e0638
CHAN W1LL <303661031@qq.com>
2023-03-08 18:36:48 +0800

    动态规划：爬楼梯问题

--- src/Algorithms/SuperStair.java
+++ src/Algorithms/SuperStair.java
@@ -9,6 +9,17 @@ public class SuperStair {
 
     }
 
+    public static int climbStairs(int n){
+        int p = 0, q = 0, r = 1;
+        for (int i = 0; i < n; i++) {
+            p = q;
+            q = r;
+            r = p + q;
+        }
+        return r;
+
+    }
+
     public static void main(String[] args) {
         System.out.println(superstair(4));
     }
