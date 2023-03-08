94ae0b84ab758a7fed269c0eeff3d28572f235d5
CHAN W1LL <303661031@qq.com>
2023-03-08 18:05:59 +0800

    给一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。

--- src/DataStructures/InvertTree.java
+++ src/DataStructures/InvertTree.java
@@ -3,6 +3,11 @@ package DataStructures;
 public class InvertTree {
 
     public TreeNode invertTree(TreeNode root) {
-
+        if (root == null) return null;
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
     }
 }
