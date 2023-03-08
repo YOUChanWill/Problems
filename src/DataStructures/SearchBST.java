9c545611bed13085cc1cfb00361b7ff723647575
CHAN W1LL <303661031@qq.com>
2023-03-08 19:11:00 +0800

    二叉搜索树中的搜索

--- src/DataStructures/SearchBST.java
+++ src/DataStructures/SearchBST.java
@@ -1,7 +1,29 @@
 package DataStructures;
 
 public class SearchBST {
-    public TreeNode searchBST(TreeNode root, int val) {
 
+// 左子树所有节点的元素值均小于根的元素值；
+// 右子树所有节点的元素值均大于根的元素值。
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (val == root.val) {
+            return root;
+        }
+        return searchBST(val < root.val ? root.left : root.right, val);
     }
+
+
+    public TreeNode searchBST01(TreeNode root, int val) {
+        while (root != null) {
+            if (val == root.val) {
+                return root;
+            }
+            root = val < root.val ? root.left : root.right;
+        }
+        return null;
+    }
+
 }
