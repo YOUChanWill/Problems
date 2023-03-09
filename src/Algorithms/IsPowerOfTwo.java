040ed8ce66a11334ae3d506a9623e8093a4a779e
CHAN W1LL <303661031@qq.com>
2023-03-09 11:44:15 +0800

    判断一个整数是否为2的幂次

---
+++ src/Algorithms/IsPowerOfTwo.java
@@ -0,0 +1,14 @@
+package Algorithms;
+
+public class IsPowerOfTwo {
+
+    public boolean isPowerOfTwo(int n) {
+        if (n<0) return false;
+        if (Integer.bitCount(n) == 1){
+            return true;
+        }else return false;
+
+    }
+
+
+}
