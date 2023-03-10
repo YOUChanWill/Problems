07eddc68b327cc120a667734cbe2c9c82e52d3d5
CHAN W1LL <303661031@qq.com>
2023-03-10 14:10:25 +0800

    二叉搜索树的最近公共祖先

---
+++ src/DataStructures/LowestCommonAncestor.java
@@ -0,0 +1,18 @@
+package DataStructures;
+
+public class LowestCommonAncestor {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode ancestor = root;
+        while (true) {
+            if (p.val < ancestor.val && q.val < ancestor.val) {
+                ancestor = ancestor.left;
+            } else if (p.val > ancestor.val && q.val > ancestor.val) {
+                ancestor = ancestor.right;
+            } else {
+                break;
+            }
+        }
+        return ancestor;
+    }
+}
