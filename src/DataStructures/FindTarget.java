38835f384e8b389fc892ed876ba663037826f12e
CHAN W1LL <303661031@qq.com>
2023-03-10 14:08:13 +0800

    两数之和：输入二叉搜索树

---
+++ src/DataStructures/FindTarget.java
@@ -0,0 +1,20 @@
+package DataStructures;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FindTarget {
+
+        Set<Integer> set = new HashSet<Integer>();
+
+        public boolean findTarget(TreeNode root, int k) {
+            if (root == null) {
+                return false;
+            }
+            if (set.contains(k - root.val)) {
+                return true;
+            }
+            set.add(root.val);
+            return findTarget(root.left, k) || findTarget(root.right, k);
+        }
+}
