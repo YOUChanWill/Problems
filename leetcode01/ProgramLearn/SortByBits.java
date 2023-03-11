7bbc3ead638056a2d4c610b934abae52d6cc7c32
CHAN W1LL <303661031@qq.com>
2023-03-11 18:45:10 +0800

    1356. 根据数字二进制下 1 的数目排序

---
+++ leetcode01/ProgramLearn/SortByBits.java
@@ -0,0 +1,18 @@
+package ProgramLearn;
+
+import java.util.Arrays;
+
+public class SortByBits {
+
+    public int[] sortByBits(int[] arr) {
+        int[] map = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            map[i] = Integer.bitCount(arr[i]) * 10000000 + arr[i];
+        }
+        Arrays.sort(map);
+        for (int i = 0; i < map.length; i++) {
+            map[i] = map[i] % 10000000;
+        }
+        return map;
+    }
+}
