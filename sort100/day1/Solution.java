40f1a5cc4b7aee224494f9a4e64920b3ae5d9e7c
CHAN W1LL <303661031@qq.com>
2023-08-01 18:59:46 +0800

    排序算法练习

--- sort100/day1/Solution.java
+++ sort100/day1/Solution.java
@@ -392,9 +392,25 @@ public class Solution {
     }
 
 
-
+    /**给你一个数组 nums，请你从中抽取一个子序列，满足该子序列的元素之和 严格 大于未包含在该子序列中的各元素之和。
+     如果存在多个解决方案，只需返回 长度最小 的子序列。如果仍然有多个解决方案，则返回 元素之和最大 的子序列。
+     与子数组不同的地方在于，「数组的子序列」不强调元素在原数组中的连续性，也就是说，它可以通过从数组中分离一些（也可能不分离）元素得到。**/
     public List<Integer> minSubsequence(int[] nums) {
-
+        ArrayList<Integer> list = new ArrayList<>();
+        Arrays.sort(nums);
+        int lengh = nums.length,sum = 0,curr = 0;
+        if (lengh == 1) list.add(nums[0]);
+        for (int i = 0; i < lengh; i++) {
+            sum += nums[i];
+        }
+        for (int i = lengh - 1; i >= 0 ; i--) {
+            if (sum > curr && lengh != 1) {
+                curr += nums[i];
+                sum = sum - nums[i];
+                list.add(nums[i]);
+            }
+        }
+        return list;
     }
 
 
