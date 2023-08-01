4cd022e46fbd2d201f49a575c00e54aaa444d25a
CHAN W1LL <303661031@qq.com>
2023-08-01 19:06:51 +0800

    排序算法练习

--- sort100/day1/Solution.java
+++ sort100/day1/Solution.java
@@ -413,8 +413,31 @@ public class Solution {
         return list;
     }
 
+    /**给你一个整数数组 nums ，请你将数组按照每个值的频率 升序 排序。如果有多个值的频率相同，请你按照数值本身将它们 降序 排序。 **/
+    public int[] frequencySort(int[] nums) {
+
+    }
 
 
 
+
+    /**请你将一些箱子装在 一辆卡车 上。给你一个二维数组 boxTypes ，其中 boxTypes[i] = [numberOfBoxesi, numberOfUnitsPerBoxi] ：
+     numberOfBoxesi 是类型 i 的箱子的数量。
+     numberOfUnitsPerBoxi 是类型 i 每个箱子可以装载的单元数量。
+     整数 truckSize 表示卡车上可以装载 箱子 的 最大数量 。只要箱子数量不超过 truckSize ，你就可以选择任意箱子装到卡车上。
+     返回卡车可以装载 单元 的 最大 总数。**/
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+
+    }
+
+
+    /**给你两个二维整数数组 items1 和 items2 ，表示两个物品集合。每个数组 items 有以下特质：
+     items[i] = [valuei, weighti] 其中 valuei 表示第 i 件物品的 价值 ，weighti 表示第 i 件物品的 重量 。
+     items 中每件物品的价值都是 唯一的 。
+     请你返回一个二维数组 ret，其中 ret[i] = [valuei, weighti]， weighti 是所有价值为 valuei 物品的 重量之和 。
+     注意：ret 应该按价值 升序 排序后返回。**/
+    public List<List<Integer>> mergeSimilarItems(int[][] items1, int[][] items2) {
+
+    }
 
 }
