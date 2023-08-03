a604622b772c83d56ff342fb6b838bbe5a7c258d
CHAN W1LL <303661031@qq.com>
2023-08-03 14:24:55 +0800

    排序算法练习

--- sort100/day3/Solution.java
+++ sort100/day3/Solution.java
@@ -1,7 +1,6 @@
 package day3;
 
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.*;
 
 public class Solution {
 
@@ -139,6 +138,76 @@ public class Solution {
         }
         return max;
     }
+
+
+    /**一个数对 (a,b) 的 数对和 等于 a + b 。最大数对和 是一个数对数组中最大的 数对和 。
+
+     比方说，如果我们有数对 (1,5) ，(2,3) 和 (4,4)，最大数对和 为 max(1+5, 2+3, 4+4) = max(6, 5, 8) = 8 。
+     给你一个长度为 偶数 n 的数组 nums ，请你将 nums 中的元素分成 n / 2 个数对，使得：
+
+     nums 中每个元素 恰好 在 一个 数对中，且
+     最大数对和 的值 最小 。
+     请你在最优数对划分的方案下，返回最小的 最大数对和 。**/
+    public int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int ans = nums[0] + nums[n - 1];
+        for (int i = 0,j = n - 1; i < j; i++,j--) {
+            ans = Math.max(ans,nums[i] + nums[j]);
+        }
+        return ans;
+    }
+
+
+
+    /**牌组中的每张卡牌都对应有一个唯一的整数。你可以按你想要的顺序对这套卡片进行排序。
+
+     最初，这些卡牌在牌组里是正面朝下的（即，未显示状态）。
+
+     现在，重复执行以下步骤，直到显示所有卡牌为止：
+
+     从牌组顶部抽一张牌，显示它，然后将其从牌组中移出。
+     如果牌组中仍有牌，则将下一张处于牌组顶部的牌放在牌组的底部。
+     如果仍有未显示的牌，那么返回步骤 1。否则，停止行动。
+     返回能以递增顺序显示卡牌的牌组顺序。
+     答案中的第一张牌被认为处于牌堆顶部。**/
+    public int[] deckRevealedIncreasing(int[] deck) {
+        int n = deck.length;
+        Deque<Integer> index = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            index.add(i);
+        }
+        int[] ans = new int[n];
+        Arrays.sort(deck);
+        for (int x :
+                deck) {
+            ans[index.pollFirst()] = x;
+            if (!index.isEmpty()) index.add(index.pollFirst());
+        }
+        return ans;
+    }
+
+    public int[] deckRevealedIncreasing01(int[] deck) {
+        if (deck == null || deck.length < 1) {
+            return deck;
+        }
+        Arrays.sort(deck);// 得到升序排列的数组
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = deck.length - 1;i >= 0;i--) {// 倒着遍历，便是按降序访问
+            queue.add(deck[i]);// 选最大值插入队列
+            if (i == 0) {// 数组中所有元素均在队列中，退出过程
+                break;
+            }
+
+            queue.add(queue.poll());// 将队头元素插入到队尾中
+        }
+        for (int i = deck.length - 1;i >= 0;i--) {
+            deck[i] = queue.poll();// 倒回去，得到answer
+        }
+        return deck;
+    }
+
 
 
 
