eeed27e0fce8f537fea95bc2bc82c6739c686b36
CHAN W1LL <303661031@qq.com>
2023-08-08 15:13:44 +0800

    算法练习

--- sort100/day8/Solution.java
+++ sort100/day8/Solution.java
@@ -229,7 +229,235 @@ public class Solution {
 
      请你找出所有满足条件的下标 i 和 j ，找出并返回 nums[i] + nums[j] 可以得到的 最大值 。**/
     public int maximumSum(int[] nums) {
-
+         /*
+        预处理位数和+HashMap统计+贪心
+        用长度为2的数组记录两个最大值
+         */
+        int n = nums.length;
+        HashMap<Integer, int[]> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int num = nums[i], sum = 0;
+            while (num != 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            // arr[0]最大值，arr[1]次大值
+            int[] arr = map.getOrDefault(sum, new int[2]);
+            if (nums[i] >= arr[0]) {
+                arr[1] = arr[0];
+                arr[0] = nums[i];
+            } else if (nums[i] > arr[1]) {
+                arr[1] = nums[i];
+            }
+            map.put(sum, arr);
+        }
+        int res = -1;
+        for (int key : map.keySet()) {
+            int[] arr = map.get(key);
+            // 跳过数位和只有一个的情况
+            if (arr[1] == 0) continue;
+            res = Math.max(res, arr[0] + arr[1]);
+        }
+        return res;
     }
+    public int maximumSum01(int[] nums) {
+        int[] num1 = new int[82];
+        int[] num2 = new int[82];
+        for(int num : nums){
+            int s = 0;
+            int x = num;
+            while(x > 0){
+                s += x %10;
+                x /= 10;
+            }
+            if(num >= num1[s]){
+                num2[s] = num1[s];
+                num1[s] = num;
+            }else if(num > num2[s]){
+                num2[s] = num;
+            }
+        }
+        int result = -1;
+        for(int i = 0; i < num1.length ; i++){
+            if(num2[i] != 0){
+                result = Math.max(result,(num1[i]+num2[i]));
+            }
+        }
+        return result;
+    }
+
+
+
+    /**有 n 名工人。 给定两个数组 quality 和 wage ，其中，quality[i] 表示第 i 名工人的工作质量，其最低期望工资为 wage[i] 。
+
+     现在我们想雇佣 k 名工人组成一个工资组。在雇佣 一组 k 名工人时，我们必须按照下述规则向他们支付工资：
+
+     对工资组中的每名工人，应当按其工作质量与同组其他工人的工作质量的比例来支付工资。
+     工资组中的每名工人至少应当得到他们的最低期望工资。
+     给定整数 k ，返回 组成满足上述条件的付费群体所需的最小金额 。在实际答案的 10-5 以内的答案将被接受。。**/
+    public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
+        int n = quality.length;
+        Integer[] indexArr = new Integer[n];
+        for(int i=0; i<n; i++){
+            indexArr[i] = i;
+        }
+        //定义 工资比质量，一份质量给一份工资，故同事都被拔高到最高待遇的同事同等待遇。排序下标顺序由低至高，先找每份质量工质要求最低的员工。
+        Arrays.sort(indexArr, (a, b) -> {
+            return quality[b]*wage[a] - quality[a]*wage[b];
+        });
+
+        double totalQuality = 0;
+        //由于我们按质量总和给钱，故维护pq降序，大的在前，大堆顶，即使淘汰高质量员工
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
+        //先塞进k-1个低要求的员工。
+        for(int i=0; i<k-1; i++){
+            int idx = indexArr[i];
+            totalQuality += quality[idx];
+            pq.offer(quality[idx]);
+        }
+        //再来找每一个要求最高的宝藏员工，由于拿到比例后，我们按质量给钱,需要及时踢出高质量员工降低成本
+        double ans = Double.MAX_VALUE;
+        for(int i=k-1; i<n; i++){
+            //高质量员工下标
+            int idx = indexArr[i];
+            //新高质量员工带来的工资质量比例
+            double bili = (double)wage[idx]/quality[idx];
+            //招进来当前的高质量员工，此时组内正好K个
+            totalQuality += quality[idx];
+            pq.offer(quality[idx]);
+            //即时更新开销，找最小
+            double nowCost = bili*totalQuality;
+            ans = Math.min(ans, nowCost);
+
+            //踢出高质量员工降低成本，为下一次招新做准备
+            totalQuality -= pq.poll();
+        }
+        return ans;
+    }
+
+    public static class Employee {
+        double rubbish;
+        int quality;
+
+        public Employee(int wage, int quality) {
+            this.rubbish = (double) wage / (double) quality;
+            this.quality = quality;
+
+        }
+    }
+
+
+    public double mincostToHireWorkers01(int[] quality, int[] wage, int k) {
+        //1.获得总工人数
+        int num = quality.length;
+        //2.创建工人对象数组
+        Employee[] employees = new Employee[num];
+        for (int i = 0; i < num; i++) {
+            employees[i] = new Employee(wage[i], quality[i]);
+        }
+        //3.按照垃圾比，从小到大排序
+        Arrays.sort(employees, (a, b) -> a.rubbish <= b.rubbish ? -1 : 1);
+        //4.创建大根堆（作为门槛）
+        PriorityQueue<Integer> heap = new PriorityQueue<>((a, b) -> b - a);
+
+        double ans = Double.MAX_VALUE;
+        for (int i = 0,sum = 0; i < num; i++) {
+            int curQuality = employees[i].quality;
+            //5.如果堆没有满
+            if (heap.size() < k) {
+                sum += curQuality;
+                heap.add(curQuality);
+                if (heap.size() == k) {
+                    ans = Math.min(ans, sum * employees[i].rubbish);
+                }
+            } else {//堆满了
+                if (heap.peek() > curQuality) {
+                    sum += curQuality - heap.poll();
+                    heap.add(curQuality);
+                    ans = Math.min(ans, sum * employees[i].rubbish);
+                }
+            }
+        }
+        return ans;
+    }
+
+    public double mincostToHireWorkers02(int[] quality, int[] wage, int k) {
+        int len = wage.length;
+        Integer[] indexs = new Integer[len];
+        for (int i = 0; i < len; i ++){
+            indexs[i] = i;
+        }
+        Arrays.sort(indexs,(a, b) -> wage[a] * quality[b] - wage[b] * quality[a]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        double ans = 1e9;
+        double totalQ = 0.0;
+        for (int i = 0; i < k-1; i ++){
+            totalQ += quality[indexs[i]];
+            pq.offer(quality[indexs[i]]);
+        }
+        for (int i = k -1; i < len; i ++){
+            int index = indexs[i];
+            totalQ += quality[index];
+            pq.offer(quality[index]);
+            double data = ((double)wage[index])/quality[index] * totalQ;
+            ans = Math.min(data, ans);
+            totalQ -= pq.poll();
+        }
+        return ans;
+    }
+
+    /**堆箱子。给你一堆n个箱子，箱子宽 wi、深 di、高 hi。箱子不能翻转，将箱子堆起来时，下面箱子的宽度、高度和深度必须大于上面的箱子。实现一种方法，搭出最高的一堆箱子。箱堆的高度为每个箱子高度的总和。
+
+     输入使用数组[wi, di, hi]表示每个箱子。**/
+    public int pileBox(int[][] box) {
+        Arrays.sort(box,(a,b)->a[0]-b[0]);
+        int[] dp = new int[box.length];
+        int res = 0;
+        for (int i = 0; i < box.length; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (box[i][0] > box[j][0] && box[i][1] > box[j][1] && box[i][2] > box[j][2])
+                    dp[i] = Math.max(dp[i],dp[j]);
+            }
+            dp[i] += box[i][2];
+            res = Math.max(dp[i],res);
+        }
+        return res;
+    }
+
+    public int pileBox01(int[][] box) {
+        int len = box.length;
+        Arrays.sort(box,(a,b) -> {
+            if(a[0] != b[0]){
+                return a[0] - b[0];
+            }
+            if(a[1] != b[1]){
+                return b[1] - a[1];
+            }
+            if(a[2] != b[2]){
+                return b[2] - a[2];
+            }
+            return 0;
+        });
+
+        int[] dp = new int[box.length];
+        dp[0] = box[0][2];
+        int res = dp[0];
+        // int max = 0;
+        for (int i= 1;i<box.length;i++){
+            dp[i] = box[i][2];
+            int depth = box[i][1];
+            int height = box[i][2];
+            for (int j= 0;j<i;j++){
+                if (box[j][1] < depth && box[j][2] < height){
+                    dp[i] = Math.max(dp[j] + height,dp[i]);
+                }
+            }
+            res = Math.max(dp[i],res);
+        }
+        return res;
+    }
+
+
+
 
 }
