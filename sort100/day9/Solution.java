9c290157d9d883725abde92879ad82b51ea613ae
CHAN W1LL <303661031@qq.com>
2023-08-09 13:09:57 +0800

    算法练习

--- sort100/day9/Solution.java
+++ sort100/day9/Solution.java
@@ -1,7 +1,6 @@
 package day9;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
 
@@ -92,9 +91,73 @@ public class Solution {
         return ret;
     }
 
-    public List<Integer> majorityElement(int[] nums) {
 
+    /**给定一个大小为 n 的整数数组，找出其中所有出现超过 ⌊ n/3 ⌋ 次的元素。**/
+    public List<Integer> majorityElement(int[] nums) {
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        for (int x :
+                nums) {
+            map.put(x,map.getOrDefault(x,0) + 1);
+        }
+        for (int x : nums) {
+            if (map.get(x) > (n / 3)) {
+                ans.add(x);
+                map.put(x,0);
+            }
+        }
+        return ans;
     }
+    // 使用摩尔投票法
+    public List<Integer> majorityElement01(int[] nums) {
+        int element1 = 0;
+        int element2 = 0;
+        int vote1 = 0;
+        int vote2 = 0;
+
+        for (int num : nums) {
+            if (vote1 > 0 && num == element1) { //如果该元素为第一个元素，则计数加1
+                vote1++;
+            } else if (vote2 > 0 && num == element2) { //如果该元素为第二个元素，则计数加1
+                vote2++;
+            } else if (vote1 == 0) { // 选择第一个元素
+                element1 = num;
+                vote1++;
+            } else if (vote2 == 0) { // 选择第二个元素
+                element2 = num;
+                vote2++;
+            } else { //如果三个元素均不相同，则相互抵消1次
+                vote1--;
+                vote2--;
+            }
+        }
+
+        int cnt1 = 0;
+        int cnt2 = 0;
+        for (int num : nums) {
+            if (vote1 > 0 && num == element1) {
+                cnt1++;
+            }
+            if (vote2 > 0 && num == element2) {
+                cnt2++;
+            }
+        }
+        // 检测元素出现的次数是否满足要求
+        List<Integer> ans = new ArrayList<>();
+        if (vote1 > 0 && cnt1 > nums.length / 3) {
+            ans.add(element1);
+        }
+        if (vote2 > 0 && cnt2 > nums.length / 3) {
+            ans.add(element2);
+        }
+
+        return ans;
+    }
+
+
+
+
 
 
 
