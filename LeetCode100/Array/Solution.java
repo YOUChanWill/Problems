bbdca276517f11d0b3fb5ebf2c12ad6bb3cd79cb
CHAN W1LL <303661031@qq.com>
2023-08-11 12:01:24 +0800

    数组、矩阵

--- LeetCode100/Array/Solution.java
+++ LeetCode100/Array/Solution.java
@@ -1,5 +1,9 @@
 package Array;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Solution {
 
 
@@ -9,7 +13,118 @@ public class Solution {
 
      请不要使用除法，且在 O(n) 时间复杂度内完成此题。**/
     public int[] productExceptSelf(int[] nums) {
+        int length = nums.length;
 
+        // L 和 R 分别表示左右两侧的乘积列表
+        int[] L = new int[length];
+        int[] R = new int[length];
+
+        int[] answer = new int[length];
+
+        // L[i] 为索引 i 左侧所有元素的乘积
+        // 对于索引为 '0' 的元素，因为左侧没有元素，所以 L[0] = 1
+        L[0] = 1;
+        for (int i = 1; i < length; i++) {
+            L[i] = nums[i - 1] * L[i - 1];
+        }
+
+        // R[i] 为索引 i 右侧所有元素的乘积
+        // 对于索引为 'length-1' 的元素，因为右侧没有元素，所以 R[length-1] = 1
+        R[length - 1] = 1;
+        for (int i = length - 2; i >= 0; i--) {
+            R[i] = nums[i + 1] * R[i + 1];
+        }
+
+        // 对于索引 i，除 nums[i] 之外其余各元素的乘积就是左侧所有元素的乘积乘以右侧所有元素的乘积
+        for (int i = 0; i < length; i++) {
+            answer[i] = L[i] * R[i];
+        }
+
+        return answer;
     }
+
+    public int[] productExceptSelf01(int[] nums) {
+        int length = nums.length;
+        int[] answer = new int[length];
+
+        // answer[i] 表示索引 i 左侧所有元素的乘积
+        // 因为索引为 '0' 的元素左侧没有元素， 所以 answer[0] = 1
+        answer[0] = 1;
+        for (int i = 1; i < length; i++) {
+            answer[i] = nums[i - 1] * answer[i - 1];
+        }
+
+        // R 为右侧所有元素的乘积
+        // 刚开始右边没有元素，所以 R = 1
+        int R = 1;
+        for (int i = length - 1; i >= 0; i--) {
+            // 对于索引 i，左边的乘积为 answer[i]，右边的乘积为 R
+            answer[i] = answer[i] * R;
+            // R 需要包含右边所有的乘积，所以计算下一个结果时需要将当前值乘到 R 上
+            R *= nums[i];
+        }
+        return answer;
+    }
+
+
+
+    /**给你一个未排序的整数数组 nums ，请你找出其中没有出现的最小的正整数。
+
+     请你实现时间复杂度为 O(n) 并且只使用常数级别额外空间的解决方案。**/
+    public int firstMissingPositive(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int max = Integer.MIN_VALUE;
+        for (int x :
+                nums) {
+            map.put(x, 1);
+            max = Math.max(max,x);
+        }
+        Arrays.sort(nums);
+        for (int i = 1; i < max; i++) {
+            if (!map.containsKey(i)) return i;
+        }
+        return max > 0 ? max + 1 : 1;
+    }
+
+    public int firstMissingPositive01(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] <= 0) {
+                nums[i] = n + 1;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            int num = Math.abs(nums[i]);
+            if (num <= n) {
+                nums[num - 1] = -Math.abs(nums[num - 1]);
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+
+    public int firstMissingPositive02(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+
+
+
 
 }
