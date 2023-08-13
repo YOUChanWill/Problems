8d98353325c4ca3bbd185dd27d2650f40fd23eec
CHAN W1LL <303661031@qq.com>
2023-08-13 14:16:03 +0800

    .

--- BinarySearch/base/TestString.java
+++
@@ -1,9 +0,0 @@
-package base;
-
-public class TestString {
-    public static void main(String[] args) {
-        String str1 = "AAABBB";
-        String str2 = new String("AAA") + new String("BBB");
-        System.out.println(str2.intern() == str2);
-    }
-}
