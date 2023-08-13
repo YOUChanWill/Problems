bbd6122f4988924655c2631ca350fd10c83103f0
CHAN W1LL <303661031@qq.com>
2023-08-13 13:49:34 +0800

    链表

--- LeetCode100/LinkedList/solution.java
+++ LeetCode100/LinkedList/solution.java
@@ -1,6 +1,39 @@
 package LinkedList;
 
 public class solution {
+    class ListNode {
+
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int x) {val = x;next = null;}
+    }
+
+
+    /**给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。**/
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+    }
+
+
+    /**给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 。**/
+    public boolean isPalindrome(ListNode head) {
+
+    }
+
+
+
+    /**给定一个链表的头节点  head ，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+
+     如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。
+     为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
+
+     如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+     不允许修改 链表。**/
+    public ListNode detectCycle(ListNode head) {
+
+    }
+
 
 
 }
