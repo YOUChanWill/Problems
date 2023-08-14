40a83785ba0d22bdfdf2772a19115d8badd90435
CHAN W1LL <303661031@qq.com>
2023-08-14 13:04:58 +0800

    二叉树

--- LeetCode100/Tree/Solution.java
+++ LeetCode100/Tree/Solution.java
@@ -2,5 +2,68 @@ package Tree;
 
 public class Solution {
 
+     class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode() {}
+         TreeNode(int val) { this.val = val; }
+         TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+         }
+    }
+
+    /**给你一棵二叉树的根节点，返回该树的 直径 。
+
+     二叉树的 直径 是指树中任意两个节点之间最长路径的 长度 。这条路径可能经过也可能不经过根节点 root 。
+
+     两节点之间路径的 长度 由它们之间边数表示。**/
+    int max = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        depth(root);
+        return max;
+    }
+
+    private int depth(TreeNode node){
+        if (node == null) return 0;
+        int left = depth(node.left);
+        int right = depth(node.right);
+        max = Math.max(max,(left + right));//将每个节点最大直径(左子树深度+右子树深度)当前最大值比较并取大者
+        return Math.max(left,right)+1;//返回节点深度
+    }
+
+
+    /**给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 高度平衡 二叉搜索树。
+
+     高度平衡 二叉树是一棵满足「每个节点的左右两个子树的高度差的绝对值不超过 1 」的二叉树。**/
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode helper(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+
+        // 总是选择中间位置左边的数字作为根节点
+        int mid = (left + right) / 2;
+
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums, left, mid - 1);
+        root.right = helper(nums, mid + 1, right);
+        return root;
+    }
+
+
+    /**给定一个二叉搜索树的根节点 root ，和一个整数 k ，请你设计一个算法查找其中第 k 个最小元素（从 1 开始计数）。**/
+    public int kthSmallest(TreeNode root, int k) {
+
+    }
+
+
+
+
 
 }
