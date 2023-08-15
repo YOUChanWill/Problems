cfbb8aacaf372dc5d3f33349626d581f2de602ae
CHAN W1LL <303661031@qq.com>
2023-08-15 14:15:36 +0800

    图

--- LeetCode100/Graph/Solution.java
+++ LeetCode100/Graph/Solution.java
@@ -173,6 +173,62 @@ public class Solution {
         onPath[s] = false;
     }
 
+    /**Trie（发音类似 "try"）或者说 前缀树 是一种树形数据结构，用于高效地存储和检索字符串数据集中的键。
+     * 这一数据结构有相当多的应用情景，例如自动补完和拼写检查。
+
+     请你实现 Trie 类：
+     Trie() 初始化前缀树对象。
+     void insert(String word) 向前缀树中插入字符串 word 。
+     boolean search(String word) 如果字符串 word 在前缀树中，返回 true（即，在检索之前已经插入）；否则，返回 false 。
+     boolean startsWith(String prefix) 如果之前已经插入的字符串 word 的前缀之一为 prefix ，返回 true ；否则，返回 false 。**/
+    class Trie {
+        class TrieNode {
+            TrieNode[] next;
+            boolean word;
+        }
+
+        TrieNode root;
+
+        public Trie() {
+            root = new TrieNode();
+        }
+
+        public void insert(String word) {
+            TrieNode curr = root;
+            for (char c: word.toCharArray()) {
+                if (curr.next == null) {
+                    curr.next = new TrieNode[26];
+                }
+                if (curr.next[c - 'a'] == null) {
+                    curr.next[c - 'a'] = new TrieNode();
+                }
+                curr = curr.next[c - 'a'];
+            }
+            curr.word = true;
+        }
+
+        public boolean search(String word) {
+            TrieNode curr = root;
+            for (char c: word.toCharArray()) {
+                if (curr.next == null || curr.next[c - 'a'] == null) {
+                    return false;
+                }
+                curr = curr.next[c - 'a'];
+            }
+            return curr.word;
+        }
+
+        public boolean startsWith(String prefix) {
+            TrieNode curr = root;
+            for (char c: prefix.toCharArray()) {
+                if (curr.next == null || curr.next[c - 'a'] == null) {
+                    return false;
+                }
+                curr = curr.next[c - 'a'];
+            }
+            return true;
+        }
+    }
 
 
 }
