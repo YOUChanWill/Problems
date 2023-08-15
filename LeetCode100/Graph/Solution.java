2ed84b628bfdb717e72c5fe548757eba1c329e25
CHAN W1LL <303661031@qq.com>
2023-08-15 14:09:12 +0800

    图

--- LeetCode100/Graph/Solution.java
+++ LeetCode100/Graph/Solution.java
@@ -1,5 +1,10 @@
 package Graph;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class Solution {
 
     /**给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
@@ -78,7 +83,95 @@ public class Solution {
     }
 
 
+    /**你这个学期必须选修 numCourses 门课程，记为 0 到 numCourses - 1 。
 
+     在选修某些课程之前需要一些先修课程。 先修课程按数组 prerequisites 给出，
+     其中 prerequisites[i] = [ai, bi] ，表示如果要学习课程 ai 则 必须 先学习课程  bi 。
+
+     例如，先修课程对 [0, 1] 表示：想要学习课程 0 ，你需要先完成课程 1 。
+     请你判断是否可能完成所有课程的学习？如果可以，返回 true ；否则，返回 false 。**/
+    List<List<Integer>> edges;
+    int[] indeg;
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        edges = new ArrayList<List<Integer>>();
+        for (int i = 0; i < numCourses; ++i) {
+            edges.add(new ArrayList<Integer>());
+        }
+        indeg = new int[numCourses];
+        for (int[] info : prerequisites) {
+            edges.get(info[1]).add(info[0]);
+            ++indeg[info[0]];
+        }
+
+        Queue<Integer> queue = new LinkedList<Integer>();
+        for (int i = 0; i < numCourses; ++i) {
+            if (indeg[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        int visited = 0;
+        while (!queue.isEmpty()) {
+            ++visited;
+            int u = queue.poll();
+            for (int v: edges.get(u)) {
+                --indeg[v];
+                if (indeg[v] == 0) {
+                    queue.offer(v);
+                }
+            }
+        }
+
+        return visited == numCourses;
+    }
+
+    boolean[] visited;
+    boolean[] onPath;
+    boolean hasCycle;
+
+    public boolean canFinish01(int numCourses, int[][] prerequisites) {
+        visited = new boolean[numCourses];
+        onPath = new boolean[numCourses];
+        hasCycle = false;
+        LinkedList<Integer>[] graph = buildGraph(numCourses, prerequisites);
+        for (int i = 0; i < numCourses; i++) {
+            traverse(numCourses, prerequisites, graph, i);
+        }
+        return !hasCycle;
+    }
+
+    LinkedList<Integer>[] buildGraph(int numCourses, int[][] prerequisites) {
+        LinkedList<Integer>[] graph = new LinkedList[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            graph[i] = new LinkedList<>();
+        }
+        for (int[] v : prerequisites) {
+            int from = v[1];
+            int to = v[0];
+            graph[from].add(to);
+        }
+        return graph;
+    }
+
+    void traverse(int numCourses, int[][] prerequisites, LinkedList<Integer>[] graph, int s) {
+        if (graph[s] == null) {
+            return;
+        }
+        if (onPath[s]) {
+            hasCycle = true;
+        }
+        if (visited[s] || hasCycle) {
+            return;
+        }
+
+        onPath[s] = true;
+        visited[s] = true;
+        for (int x : graph[s]) {
+            traverse(numCourses, prerequisites, graph, x);
+        }
+
+        onPath[s] = false;
+    }
 
 
 
