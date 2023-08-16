3dc6357ceb703a62b8a1b1987eadd4ed433ceba3
CHAN W1LL <303661031@qq.com>
2023-08-16 11:37:53 +0800

    每日一题、周赛

--- 每日一题、周赛/easy/Solution.java
+++ 每日一题、周赛/easy/Solution.java
@@ -21,7 +21,18 @@ public class Solution {
 
      给你参与游戏的朋友数量 n 和一个整数 k ，请按升序排列返回包含所有输家编号的数组 answer 作为答案。**/
     public int[] circularGameLosers(int n, int k) {
+        boolean[] vis = new boolean[n];
+        int m = n;
 
+        for (int i = 0, d = k; !vis[i]; d += k, m--) {
+            vis[i] = true;
+            i = (i + d) % n;
+        }
+        int[] ans = new int[m];
+        for (int i = 0, j = 0; i < n; i++) {
+            if (!vis[i]) ans[j++] = i + 1;
+        }
+        return ans;
     }
 
 
