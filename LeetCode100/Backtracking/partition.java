1eb683a179a2214783ae488a8036f7e9fe9d0e48
CHAN W1LL <303661031@qq.com>
2023-08-16 12:50:27 +0800

    131. 分割回文串

--- LeetCode100/Backtracking/partition.java
+++ LeetCode100/Backtracking/partition.java
@@ -1,5 +1,6 @@
 package Backtracking;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class partition {
@@ -7,7 +8,45 @@ public class partition {
     /**给你一个字符串 s，请你将 s 分割成一些子串，使每个子串都是 回文串 。返回 s 所有可能的分割方案。
 
      回文串 是正着读和反着读都一样的字符串。**/
-    public List<List<String>> partition(String s) {
+    int[][] f;
+    List<List<String>> ret = new ArrayList<List<String>>();
+    List<String> ans = new ArrayList<String>();
+    int n;
 
+    public List<List<String>> partition(String s) {
+        n = s.length();
+        f = new int[n][n];
+
+        dfs(s, 0);
+        return ret;
+    }
+
+    public void dfs(String s, int i) {
+        if (i == n) {
+            ret.add(new ArrayList<String>(ans));
+            return;
+        }
+        for (int j = i; j < n; ++j) {
+            if (isPalindrome(s, i, j) == 1) {
+                ans.add(s.substring(i, j + 1));
+                dfs(s, j + 1);
+                ans.remove(ans.size() - 1);
+            }
+        }
+    }
+
+    // 记忆化搜索中，f[i][j] = 0 表示未搜索，1 表示是回文串，-1 表示不是回文串
+    public int isPalindrome(String s, int i, int j) {
+        if (f[i][j] != 0) {
+            return f[i][j];
+        }
+        if (i >= j) {
+            f[i][j] = 1;
+        } else if (s.charAt(i) == s.charAt(j)) {
+            f[i][j] = isPalindrome(s, i + 1, j - 1);
+        } else {
+            f[i][j] = -1;
+        }
+        return f[i][j];
     }
 }
