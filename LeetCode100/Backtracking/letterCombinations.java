9e0b6296f9fa406138d85de0fb632989e1108c6f
CHAN W1LL <303661031@qq.com>
2023-08-16 13:01:10 +0800

    17. 电话号码的字母组合

---
+++ LeetCode100/Backtracking/letterCombinations.java
@@ -0,0 +1,72 @@
+package Backtracking;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class letterCombinations {
+
+    /**给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+
+     给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。**/
+    public List<String> letterCombinations(String digits) {
+        List<String> combinations = new ArrayList<String>();
+        if (digits.length() == 0) {
+            return combinations;
+        }
+        Map<Character, String> phoneMap = new HashMap<Character, String>() {{
+            put('2', "abc");
+            put('3', "def");
+            put('4', "ghi");
+            put('5', "jkl");
+            put('6', "mno");
+            put('7', "pqrs");
+            put('8', "tuv");
+            put('9', "wxyz");
+        }};
+        backtrack(combinations, phoneMap, digits, 0, new StringBuffer());
+        return combinations;
+    }
+
+    public void backtrack(List<String> combinations, Map<Character, String> phoneMap, String digits, int index, StringBuffer combination) {
+        if (index == digits.length()) {
+            combinations.add(combination.toString());
+        } else {
+            char digit = digits.charAt(index);
+            String letters = phoneMap.get(digit);
+            int lettersCount = letters.length();
+            for (int i = 0; i < lettersCount; i++) {
+                combination.append(letters.charAt(i));
+                backtrack(combinations, phoneMap, digits, index + 1, combination);
+                combination.deleteCharAt(index);
+            }
+        }
+    }
+
+
+    List<String> list = new ArrayList<>();
+    public List<String> letterCombinations01(String digits) {
+        if (digits == null || digits.length() == 0) {
+            return list;
+        }
+        String[] numString = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        backTracking(digits, numString, 0);
+        return list;
+    }
+    StringBuilder temp = new StringBuilder();
+
+    public void backTracking(String digits, String[] numString, int num) {
+        //遍历一次记录一次得到的字符串
+        if (num == digits.length()) {
+            list.add(temp.toString());
+            return;
+        }
+        String str = numString[digits.charAt(num) - '0'];
+        for (int i = 0; i < str.length(); i++) {
+            temp.append(str.charAt(i));
+            backTracking(digits, numString, num + 1);
+            temp.deleteCharAt(temp.length() - 1);
+        }
+    }
+}
