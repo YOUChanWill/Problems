05e3c82deb4b49274e386fb11e0a71c0d5e3efa1
CHAN W1LL <303661031@qq.com>
2023-08-17 12:18:36 +0800

    二分查找

--- LeetCode100/BinarySearch/Solution.java
+++ LeetCode100/BinarySearch/Solution.java
@@ -1,4 +1,86 @@
 package BinarySearch;
 
 public class Solution {
+
+
+    /**给你一个满足下述两条属性的 m x n 整数矩阵：
+
+     每行中的整数从左到右按非递减顺序排列。
+     每行的第一个整数大于前一行的最后一个整数。
+     给你一个整数 target ，如果 target 在矩阵中，返回 true ；否则，返回 false 。**/
+    public boolean searchMatrix(int[][] mat, int t) {
+        int m = mat.length, n = mat[0].length;
+
+        // 第一次二分：定位到所在行（从上往下，找到最后一个满足 mat[x]][0] <= t 的行号）
+        int l = 0, r = m - 1;
+        while (l < r) {
+            int mid = l + r + 1 >> 1;
+            if (mat[mid][0] <= t) {
+                l = mid;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        int row = r;
+        if (mat[row][0] == t) return true;
+        if (mat[row][0] > t) return false;
+
+        // 第二次二分：从所在行中定位到列（从左到右，找到最后一个满足 mat[row][x] <= t 的列号）
+        l = 0; r = n - 1;
+        while (l < r) {
+            int mid = l + r + 1 >> 1;
+            if (mat[row][mid] <= t) {
+                l = mid;
+            } else {
+                r = mid - 1;
+            }
+        }
+        int col = r;
+
+        return mat[row][col] == t;
+    }
+
+
+    /**给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+
+     如果数组中不存在目标值 target，返回 [-1, -1]。
+
+     你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。**/
+    public int[] searchRange(int[] nums, int target) {
+        int leftIdx = binarySearch(nums, target, true);
+        int rightIdx = binarySearch(nums, target, false) - 1;
+        if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] == target && nums[rightIdx] == target) {
+            return new int[]{leftIdx, rightIdx};
+        }
+        return new int[]{-1, -1};
+    }
+
+    public int binarySearch(int[] nums, int target, boolean lower) {
+        int left = 0, right = nums.length - 1, ans = nums.length;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    /**整数数组 nums 按升序排列，数组中的值 互不相同 。
+
+     在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为 [4,5,6,7,0,1,2] 。
+
+     给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回 -1 。
+
+     你必须设计一个时间复杂度为 O(log n) 的算法解决此问题。**/
+    public int search(int[] nums, int target) {
+
+    }
+
+
+
 }
