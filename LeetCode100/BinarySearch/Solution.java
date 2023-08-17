ec2a2803e13ac431fd10645080262548cf9138b6
CHAN W1LL <303661031@qq.com>
2023-08-17 12:28:57 +0800

    二分查找

--- LeetCode100/BinarySearch/Solution.java
+++ LeetCode100/BinarySearch/Solution.java
@@ -72,12 +72,59 @@ public class Solution {
 
     /**整数数组 nums 按升序排列，数组中的值 互不相同 。
 
-     在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为 [4,5,6,7,0,1,2] 。
+     在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，
+     使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。
+     例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为 [4,5,6,7,0,1,2] 。
 
      给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回 -1 。
 
      你必须设计一个时间复杂度为 O(log n) 的算法解决此问题。**/
     public int search(int[] nums, int target) {
+        int len = nums.length;
+        if(len == 0) return -1;
+        int left = 0, right = len - 1;
+        // 1. 旋转数组后，从中间划分，一定有一边是有序的。
+        // 2. 由于一定有一边是有序的，所以根据有序的两个边界值来判断目标值在有序一边还是无序一边
+        // 3. 这题找目标值，遇到目标值即返回
+        // 4. 注意：由于有序的一边的边界值可能等于目标值，所以判断目标值是否在有序的那边时应该加个等号
+        // (在二分查找某个具体值得时候如果把握不好边界值，可以再每次查找前判断下边界值，也就是while循环里面的两个if注释)
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target) return mid;
+            // 右边有序
+            if(nums[mid] < nums[right]){
+                // 目标值在右边
+                if(target > nums[mid] && target <= nums[right]){
+                    left = mid + 1;
+                    // 目标值在左边
+                }else{
+                    right = mid - 1;
+                }
+                // 左边有序
+            }else{
+                // 目标值在左边
+                if(target >= nums[left] && target < nums[mid]){
+                    right = mid - 1;
+                    // 目标值在右边
+                }else{
+                    left = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+
+
+    /**已知一个长度为 n 的数组，预先按照升序排列，经由 1 到 n 次 旋转 后，得到输入数组。例如，原数组 nums = [0,1,2,4,5,6,7] 在变化后可能得到：
+     若旋转 4 次，则可以得到 [4,5,6,7,0,1,2]
+     若旋转 7 次，则可以得到 [0,1,2,4,5,6,7]
+     注意，数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], ..., a[n-2]] 。
+
+     给你一个元素值 互不相同 的数组 nums ，它原来是一个升序排列的数组，并按上述情形进行了多次旋转。请你找出并返回数组中的 最小元素 。
+
+     你必须设计一个时间复杂度为 O(log n) 的算法解决此问题。**/
+    public int findMin(int[] nums) {
 
     }
 
