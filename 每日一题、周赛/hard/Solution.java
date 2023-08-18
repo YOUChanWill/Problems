985b2628588b15cbad24ddd3eb70c4a77b51a29a
CHAN W1LL <303661031@qq.com>
2023-08-18 13:09:29 +0800

    每日一题、周赛

--- 每日一题、周赛/hard/Solution.java
+++ 每日一题、周赛/hard/Solution.java
@@ -1,5 +1,7 @@
 package hard;
 
+import java.util.Arrays;
+
 public class Solution {
 
 
@@ -131,6 +133,45 @@ public class Solution {
 
         }
         return minSum1;
+    }
+
+
+    /**给你一个披萨，它由 3n 块不同大小的部分组成，现在你和你的朋友们需要按照如下规则来分披萨：
+
+     你挑选 任意 一块披萨。
+     Alice 将会挑选你所选择的披萨逆时针方向的下一块披萨。
+     Bob 将会挑选你所选择的披萨顺时针方向的下一块披萨。
+     重复上述过程直到没有披萨剩下。
+     每一块披萨的大小按顺时针方向由循环数组 slices 表示。
+
+     请你返回你可以获得的披萨大小总和的最大值。**/
+    public int maxSizeSlices(int[] slices) {
+        int[] v1 = new int[slices.length - 1];
+        int[] v2 = new int[slices.length - 1];
+        System.arraycopy(slices, 1, v1, 0, slices.length - 1);
+        System.arraycopy(slices, 0, v2, 0, slices.length - 1);
+        int ans1 = calculate(v1);
+        int ans2 = calculate(v2);
+        return Math.max(ans1, ans2);
+    }
+
+    public int calculate(int[] slices) {
+        int N = slices.length, n = (N + 1) / 3;
+        int[][] dp = new int[N][n + 1];
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(dp[i], Integer.MIN_VALUE);
+        }
+        dp[0][0] = 0;
+        dp[0][1] = slices[0];
+        dp[1][0] = 0;
+        dp[1][1] = Math.max(slices[0], slices[1]);
+        for (int i = 2; i < N; i++) {
+            dp[i][0] = 0;
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i - 2][j - 1] + slices[i]);
+            }
+        }
+        return dp[N - 1][n];
     }
 
 
