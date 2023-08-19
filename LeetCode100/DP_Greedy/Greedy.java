f81a3f8254aac9bac0b07f8ec59e80f86fa713de
CHAN W1LL <303661031@qq.com>
2023-08-19 09:55:05 +0800

    贪心算法

--- LeetCode100/DP_Greedy/Greedy.java
+++ LeetCode100/DP_Greedy/Greedy.java
@@ -3,7 +3,7 @@ package DP_Greedy;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Solution {
+public class Greedy {
 
     /**给你一个非负整数数组 nums ，你最初位于数组的 第一个下标 。数组中的每个元素代表你在该位置可以跳跃的最大长度。
 
