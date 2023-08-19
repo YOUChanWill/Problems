9f2f771bec1a547c24718e666ab0941e75c1c2a9
CHAN W1LL <303661031@qq.com>
2023-08-19 11:02:16 +0800

    动态规划

--- LeetCode100/DP_Greedy/DP.java
+++ LeetCode100/DP_Greedy/DP.java
@@ -42,7 +42,18 @@ public class DP {
 
      你可以认为每种硬币的数量是无限的。*/
     public int coinChange(int[] coins, int amount) {
-
+        int max = amount + 1;
+        int[] dp = new int[max];
+        Arrays.fill(dp,max);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 
 
