31cdcdc88f90ca56a3a61da0b1eb6848e8b03271
CHAN W1LL <303661031@qq.com>
2023-08-20 11:50:33 +0800

    多维动态规划

--- LeetCode100/DP_Greedy/Mul_DP.java
+++ LeetCode100/DP_Greedy/Mul_DP.java
@@ -1,4 +1,62 @@
 package DP_Greedy;
 
 public class Mul_DP {
+
+    /**一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+
+     机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+
+     问总共有多少条不同的路径？**/
+    public int uniquePaths(int m, int n) {
+
+    }
+
+
+    /**给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+
+     说明：每次只能向下或者向右移动一步。**/
+    public int minPathSum(int[][] grid) {
+
+    }
+
+
+    /**给你一个字符串 s，找到 s 中最长的回文子串。
+
+     如果字符串的反序与原始字符串相同，则该字符串称为回文字符串。*/
+    public String longestPalindrome(String s) {
+
+    }
+
+
+    /**给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
+
+     一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+
+     例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
+     两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
+     **/
+    public int longestCommonSubsequence(String text1, String text2) {
+
+    }
+
+
+
+
+    /**给你两个单词 word1 和 word2， 请返回将 word1 转换成 word2 所使用的最少操作数  。
+
+     你可以对一个单词进行如下三种操作：
+
+     插入一个字符
+     删除一个字符
+     替换一个字符*/
+    public int minDistance(String word1, String word2) {
+
+    }
+
+
+
+
+
+
+
 }
