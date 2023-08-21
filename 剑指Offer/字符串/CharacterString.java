3a1662c90d56b4849525534df4c4b42f044d6f41
CHAN W1LL <303661031@qq.com>
2023-08-21 09:30:40 +0800

    剑指Offer：字符串

--- 剑指Offer/字符串/CharacterString.java
+++ 剑指Offer/字符串/CharacterString.java
@@ -1,10 +1,12 @@
-public class String {
+package 字符串;
+
+public class CharacterString {
 
     /**剑指 Offer 05. 替换空格
 
      请实现一个函数，把字符串 s 中的每个空格替换成"%20"。**/
     public String replaceSpace(String s) {
-
+        return s.replaceAll(" ","%20");
     }
 
 
@@ -71,7 +73,4 @@ public class String {
     public int strToInt(String str) {
 
     }
-
-
-
 }
