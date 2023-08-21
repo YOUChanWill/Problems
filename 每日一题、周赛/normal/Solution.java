0db13b7981fbc2638c18875d69a0d0c8548f3ea7
CHAN W1LL <303661031@qq.com>
2023-08-21 09:20:55 +0800

    每日一题、周赛

--- 每日一题、周赛/normal/Solution.java
+++ 每日一题、周赛/normal/Solution.java
@@ -232,6 +232,48 @@ public class Solution {
     }
 
 
+    /**给你两个字符串 start 和 target ，长度均为 n 。每个字符串 仅 由字符 'L'、'R' 和 '_' 组成，其中：
+
+     字符 'L' 和 'R' 表示片段，其中片段 'L' 只有在其左侧直接存在一个 空位 时才能向 左 移动，而片段 'R' 只有在其右侧直接存在一个 空位 时才能向 右 移动。
+     字符 '_' 表示可以被 任意 'L' 或 'R' 片段占据的空位。
+     如果在移动字符串 start 中的片段任意次之后可以得到字符串 target ，返回 true ；否则，返回 false 。**/
+    public boolean canChange(String start, String target) {
+        //无论怎么移动，由于 L 和 R 无法互相穿过对方，那么去掉 _ 后的剩余字符应该是相同的，否则返回 false。
+        if (!start.replaceAll("_","").equals(target.replaceAll("_",""))) return false;
+        for (int i = 0, j = 0; i < start.length(); i++) {
+            if (start.charAt(i) == '_') continue;
+            while (target.charAt(j) == '_') j++;
+            // 如果当前字符为 R 且 i>j，由于 R 由于无法向左移动，返回 false；
+            if (i != j && (start.charAt(i) == 'R') == (i > j)) return false;
+            // 如果当前字符为 L 且 i<j，由于 L 由于无法向右移动，返回 false；
+            if (i != j && (start.charAt(i) == 'L') == (i < j)) return false;
+            ++j;
+        }
+        return true;
+    }
+
+    public boolean canChange01(String start, String target) {
+        int n=start.length();
+        int i=0,j=0;
+        while(j<n){
+            if(target.charAt(j)=='L'){
+                while(i<n && start.charAt(i)=='_') i++;
+                if(i>=n || i<j || start.charAt(i)=='R') return false;
+                j++;
+                i++;
+            }else if(target.charAt(j)=='R'){
+                while(i<n&&start.charAt(i)=='_')    i++;
+                if(i>=n || i>j || start.charAt(i)=='L') return false;
+                j++;
+                i++;
+            }else j++;
+        }
+        while(i<n){
+            if(start.charAt(i++)!='_')  return false;
+        }
+        return true;
+    }
+
 
 
 
