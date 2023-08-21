53d72f38c24a0048c404d0a4edea78b83b9623c5
CHAN W1LL <303661031@qq.com>
2023-08-21 10:34:20 +0800

    剑指Offer：字符串

--- 剑指Offer/字符串/CharacterString.java
+++ 剑指Offer/字符串/CharacterString.java
@@ -23,7 +23,18 @@ public class CharacterString {
      字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。
      请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。**/
     public String reverseLeftWords(String s, int n) {
+        StringBuilder ans = new StringBuilder();
+        for (int i = n; i < s.length(); i++) {
+            ans.append(s.charAt(i));
+        }
+        for (int i = 0; i < n; i++) {
+            ans.append(s.charAt(i));
+        }
+        return ans.toString();
+    }
 
+    public String reverseLeftWords01(String s, int n) {
+        return s.substring(n,s.length()) + s.substring(0,n);
     }
 
 
