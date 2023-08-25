2ec3bc24641a9a6cdb46c0bd576d5c1dece9c75d
CHAN W1LL <303661031@qq.com>
2023-08-25 09:36:22 +0800

    剑指Offer：栈与队列

--- 剑指Offer/栈_队列/Stack_Queue.java
+++ 剑指Offer/栈_队列/Stack_Queue.java
@@ -1,5 +1,7 @@
 package 栈_队列;
 
+import java.util.Stack;
+
 public class Stack_Queue {
 
 
@@ -9,16 +11,25 @@ public class Stack_Queue {
      分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )*/
     class CQueue {
 
-        public CQueue() {
+        Stack<Integer> stack1, stack2;
 
+        public CQueue() {
+            stack1 = new Stack<>();
+            stack2 = new Stack<>();
         }
 
         public void appendTail(int value) {
-
+            stack1.push(value);
         }
 
         public int deleteHead() {
-
+            if (stack2.isEmpty()){
+                if (stack1.isEmpty()) return -1;
+                while (!stack1.isEmpty()){
+                    stack2.push(stack1.pop());
+                }
+            }
+            return stack2.pop();
         }
     }
 
