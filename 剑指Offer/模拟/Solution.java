6dba8a0f826a4375be61518a39cc73aa0eaf5647
CHAN W1LL <303661031@qq.com>
2023-08-25 14:53:27 +0800

    剑指Offer：模拟

---
+++ 剑指Offer/模拟/Solution.java
@@ -0,0 +1,20 @@
+package 模拟;
+
+public class Solution {
+
+
+    /**剑指 Offer 29. 顺时针打印矩阵
+
+     输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字。*/
+    public int[] spiralOrder(int[][] matrix) {
+
+    }
+
+
+
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+
+    }
+
+
+}
