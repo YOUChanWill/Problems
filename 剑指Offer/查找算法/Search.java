0e7cc2e87ae1b295701def3ebf0e7c618b97517f
CHAN W1LL <303661031@qq.com>
2023-08-27 13:58:24 +0800

    剑指Offer：查找算法

--- 剑指Offer/查找算法/Search.java
+++ 剑指Offer/查找算法/Search.java
@@ -54,7 +54,20 @@ public class Search {
      一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0～n-1之内。
      在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字。*/
     public int missingNumber(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i) return i;
+        }
+        return nums.length;
+    }
 
+    public int missingNumber1(int[] nums) {
+        int i = 0, j = nums.length - 1;
+        while(i <= j) {
+            int m = (i + j) / 2;
+            if(nums[m] == m) i = m + 1;
+            else j = m - 1;
+        }
+        return i;
     }
 
 
