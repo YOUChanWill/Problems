14e4357984a29d9f120cefc08f001f3683136746
CHAN W1LL <303661031@qq.com>
2023-08-27 13:46:16 +0800

    剑指Offer：查找算法

--- 剑指Offer/查找算法/Search.java
+++ 剑指Offer/查找算法/Search.java
@@ -36,10 +36,17 @@ public class Search {
     }
 
     public int search01(int[] nums, int target) {
-
+        return helper(nums, target) - helper(nums, target - 1);
     }
-
-
+    int helper(int[] nums, int tar) {
+        int i = 0, j = nums.length - 1;
+        while(i <= j) {
+            int m = (i + j) / 2;
+            if(nums[m] <= tar) i = m + 1;
+            else j = m - 1;
+        }
+        return i;
+    }
 
 
     /**剑指 Offer 53 - II. 0～n-1中缺失的数字
