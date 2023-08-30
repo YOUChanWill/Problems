57e18cdd221e094688872cb1099863aaac2f3844
CHAN W1LL <303661031@qq.com>
2023-08-30 10:52:51 +0800

    剑指Offer：搜索和回溯算法

--- 剑指Offer/搜索与回溯算法/Backtracking.java
+++ 剑指Offer/搜索与回溯算法/Backtracking.java
@@ -232,8 +232,25 @@ public class Backtracking {
      单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。
      同一个单元格内的字母不允许被重复使用。*/
     public boolean exist(char[][] board, String word) {
-
+        char[] chars = word.toCharArray();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (dfs(board, chars, i, j, 0)) return true;
+            }
+        }
+        return false;
     }
+
+    private boolean dfs(char[][] board, char[] word, int i, int j, int k){
+        if (i >= board.length || i < 0 || j >= board[0].length || j < 0 || board[i][j] != word[k]) return false;
+        if (k == (word.length - 1)) return true; // 递归结束
+        board[i][j] = '\0';
+        boolean res = dfs(board,word,i + 1, j , k + 1) || dfs(board,word,i - 1 ,j ,k + 1) ||
+                dfs(board,word,i , j + 1 , k + 1) || dfs(board,word,i  ,j - 1,k + 1);
+        board[i][j] = word[k];
+        return res;
+    }
+
 
 
     /**剑指 Offer 13. 机器人的运动范围
