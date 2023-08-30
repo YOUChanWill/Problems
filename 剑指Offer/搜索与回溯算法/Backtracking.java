1c9403074934160a159a93b4bc4062fda5aa4a65
CHAN W1LL <303661031@qq.com>
2023-08-30 13:13:18 +0800

    剑指Offer：搜索和回溯算法

--- 剑指Offer/搜索与回溯算法/Backtracking.java
+++ 剑指Offer/搜索与回溯算法/Backtracking.java
@@ -298,10 +298,23 @@ public class Backtracking {
      给你二叉树的根节点 root 和一个整数目标和 targetSum ，找出所有 从根节点到叶子节点 路径总和等于给定目标和的路径。
 
      叶子节点 是指没有子节点的节点。*/
-    public List<List<Integer>> pathSum(TreeNode root, int target) {
 
+    LinkedList<Integer> path = new LinkedList<>();
+    LinkedList<List<Integer>> ans = new LinkedList<>();
+    public List<List<Integer>> pathSum(TreeNode root, int target) {
+        pathSumDFS(root, target);
+        return ans;
     }
 
+    private void pathSumDFS(TreeNode root, int target){
+        if (root == null) return;
+        path.add(root.val);
+        target = target - root.val;
+        if (root.right == null && root.left == null && target == 0) ans.add(new LinkedList<>(path));
+        pathSumDFS(root.left,target);
+        pathSumDFS(root.right,target);
+        path.removeLast();
+    }
 
 
     /**剑指 Offer 36. 二叉搜索树与双向链表
