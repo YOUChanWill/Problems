09d88c382e3d06fa4768fc96e3cbea9bf6a053e2
CHAN W1LL <303661031@qq.com>
2023-08-31 10:15:19 +0800

    剑指Offer：搜索和回溯算法

--- 剑指Offer/搜索与回溯算法/Backtracking.java
+++ 剑指Offer/搜索与回溯算法/Backtracking.java
@@ -337,10 +337,27 @@ public class Backtracking {
     /**剑指 Offer 55 - I. 二叉树的深度
 
      输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。*/
-    public int maxDepth(TreeNode root) {
-
+    public int maxDepth1(TreeNode root) {
+        if (root == null) return 0;
+        int count = 0;
+        Deque<TreeNode> deque = new LinkedList<>();
+        deque.offerLast(root);
+        while (!deque.isEmpty()){
+            int lengh = deque.size();
+            for (int i = 0; i < lengh; i++) {
+                TreeNode node = deque.pollFirst();
+                if (node.left != null) deque.offerLast(node.left);
+                if (node.right != null) deque.offerLast(node.right);
+            }
+            count++;
+        }
+        return count;
     }
 
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(maxDepth(root.left),maxDepth(root.right)) + 1;
+    }
 
 
     /**剑指 Offer 55 - II. 平衡二叉树
