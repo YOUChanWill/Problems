31f1b16bb755f85766cf41ab108f9c3aa2227566
CHAN W1LL <303661031@qq.com>
2023-08-31 10:48:23 +0800

    剑指Offer：搜索和回溯算法

--- 剑指Offer/搜索与回溯算法/Backtracking.java
+++ 剑指Offer/搜索与回溯算法/Backtracking.java
@@ -320,18 +320,34 @@ public class Backtracking {
     /**剑指 Offer 36. 二叉搜索树与双向链表
 
      输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的循环双向链表。要求不能创建任何新的节点，只能调整树中节点指针的指向。*/
+    Node pre, head;
     public Node treeToDoublyList(Node root) {
-
+        if (root == null) return null;
+        treeToDoublyListDFS(root);
+        head.left = pre;
+        pre.right = head;
+        return head;
     }
 
+    private void treeToDoublyListDFS(Node cur){
+        if (cur == null) return;
+        treeToDoublyListDFS(cur.left);
+        if (pre != null) pre.right = cur;
+        else head = cur;
+        cur.left = pre;
+        pre = cur;
+        treeToDoublyListDFS(cur.right);
+    }
 
 
     /**剑指 Offer 54. 二叉搜索树的第k大节点
 
      给定一棵二叉搜索树，请找出其中第 k 大的节点的值。*/
     public int kthLargest(TreeNode root, int k) {
+        if (root == null) return 0;
 
     }
+
 
 
     /**剑指 Offer 55 - I. 二叉树的深度
