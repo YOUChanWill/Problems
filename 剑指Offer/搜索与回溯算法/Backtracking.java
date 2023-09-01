44e78ed26c178d185118c3cf889089f4a756e6dc
CHAN W1LL <303661031@qq.com>
2023-09-01 08:35:53 +0800

    剑指Offer：搜索和回溯算法

--- 剑指Offer/搜索与回溯算法/Backtracking.java
+++ 剑指Offer/搜索与回溯算法/Backtracking.java
@@ -393,7 +393,13 @@ public class Backtracking {
 
      输入一棵二叉树的根节点，判断该树是不是平衡二叉树。如果某二叉树中任意节点的左右子树的深度相差不超过1，那么它就是一棵平衡二叉树。*/
     public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        return Math.abs(Depth(root.left) - Depth(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
 
+    public int Depth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(Depth(root.left),Depth(root.right)) + 1;
     }
 
 
