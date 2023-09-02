9bfde3f7f4a06eb16dcdda89e0f3dbabf318939a
CHAN W1LL <303661031@qq.com>
2023-09-02 10:43:58 +0800

    剑指Offer：排序

--- 剑指Offer/排序/Sort.java
+++ 剑指Offer/排序/Sort.java
@@ -10,7 +10,14 @@ public class Sort {
 
      输入一个非负整数数组，把数组里所有数字拼接起来排成一个数，打印能拼接出的所有数字中最小的一个。*/
     public String minNumber(int[] nums) {
-
+        String[] strs = new String[nums.length];
+        for(int i = 0; i < nums.length; i++)
+            strs[i] = String.valueOf(nums[i]);
+        Arrays.sort(strs, (x, y) -> (x + y).compareTo(y + x));
+        StringBuilder res = new StringBuilder();
+        for(String s : strs)
+            res.append(s);
+        return res.toString();
     }
 
 
