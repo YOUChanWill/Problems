619c10ea45b13b3c6f1703d1bffa2a4702e4f261
CHAN W1LL <303661031@qq.com>
2023-09-02 11:15:25 +0800

    剑指Offer：位运算、数学

--- 剑指Offer/位运算_数学/Math_Bit.java
+++ 剑指Offer/位运算_数学/Math_Bit.java
@@ -27,5 +27,22 @@ public class Math_Bit {
     }
 
 
+    /**剑指 Offer 56 - I. 数组中数字出现的次数
+
+     一个整型数组 nums 里除两个数字之外，其他数字都出现了两次。请写程序找出这两个只出现一次的数字。
+     要求时间复杂度是O(n)，空间复杂度是O(1)。*/
+    public int[] singleNumbers(int[] nums) {
+
+    }
+
+
+    /**剑指 Offer 56 - II. 数组中数字出现的次数 II
+
+     在一个数组 nums 中除一个数字只出现一次之外，其他数字都出现了三次。请找出那个只出现一次的数字。*/
+    public int singleNumber(int[] nums) {
+
+    }
+
+
 
 }
