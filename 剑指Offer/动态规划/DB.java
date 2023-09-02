55ffc39e658e103b8038416cc487b2489a6a02ff
CHAN W1LL <303661031@qq.com>
2023-09-02 11:00:09 +0800

    剑指Offer：动态规划

--- 剑指Offer/动态规划/DB.java
+++ 剑指Offer/动态规划/DB.java
@@ -79,5 +79,21 @@ public class DB {
     }
 
 
+    /**剑指 Offer 49. 丑数
+
+     我们把只包含质因子 2、3 和 5 的数称作丑数（Ugly Number）。求按从小到大的顺序的第 n 个丑数。*/
+    public int nthUglyNumber(int n) {
+
+    }
+
+
+    /**剑指 Offer 60. n个骰子的点数
+
+     把n个骰子扔在地上，所有骰子朝上一面的点数之和为s。输入n，打印出s的所有可能的值出现的概率。
+
+     你需要用一个浮点数数组返回答案，其中第 i 个元素代表这 n 个骰子所能掷出的点数集合中第 i 小的那个的概率。*/
+    public double[] dicesProbability(int n) {
+
+    }
 
 }
