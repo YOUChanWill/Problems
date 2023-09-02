d2352c93dfabb66dccb6b30c413338cf1771f229
CHAN W1LL <303661031@qq.com>
2023-09-02 11:08:54 +0800

    剑指Offer：分治算法

---
+++ 剑指Offer/分治算法/Solution.java
@@ -0,0 +1,30 @@
+package 分治算法;
+
+public class Solution {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    /**剑指 Offer 07. 重建二叉树
+
+     输入某二叉树的前序遍历和中序遍历的结果，请构建该二叉树并返回其根节点。
+
+     假设输入的前序遍历和中序遍历的结果中都不含重复的数字。*/
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+    }
+
+
+
+    /**剑指 Offer 16. 数值的整数次方
+
+     实现 pow(x, n) ，即计算 x 的 n 次幂函数（即，xn）。不得使用库函数，同时不需要考虑大数问题。*/
+    public double myPow(double x, int n) {
+
+    }
+
+}
