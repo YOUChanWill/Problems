00c046f47c066d9cf87b6ebdf86b8dde3833b0e9
CHAN W1LL <303661031@qq.com>
2023-09-04 10:43:20 +0800

    剑指Offer：动态规划

--- 剑指Offer/动态规划/DB.java
+++ 剑指Offer/动态规划/DB.java
@@ -71,7 +71,12 @@ public class DB {
 
      要求时间复杂度为O(n)。*/
     public int maxSubArray(int[] nums) {
-
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += Math.max(nums[i - 1],0);
+            res = Math.max(res,nums[i]);
+        }
+        return res;
     }
 
 
