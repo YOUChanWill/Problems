b7b59e5a483d4d846e8d152cea1bdbb2b690f02a
CHAN W1LL <303661031@qq.com>
2023-09-05 09:39:13 +0800

    剑指Offer：动态规划

--- 剑指Offer/动态规划/DB.java
+++ 剑指Offer/动态规划/DB.java
@@ -1,5 +1,8 @@
 package 动态规划;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DB {
 
     /**剑指 Offer 10- I. 斐波那契数列
@@ -159,8 +162,41 @@ public class DB {
 
      请从字符串中找出一个最长的不包含重复字符的子字符串，计算该最长子字符串的长度。*/
     public int lengthOfLongestSubstring(String s) {
-
+        Map<Character, Integer> dic = new HashMap<>();
+        int res = 0, tmp = 0;
+        for(int j = 0; j < s.length(); j++) {
+            int i = dic.getOrDefault(s.charAt(j), -1); // 获取索引 i
+            dic.put(s.charAt(j), j); // 更新哈希表
+            tmp = tmp < j - i ? tmp + 1 : j - i; // dp[j - 1] -> dp[j]
+            res = Math.max(res, tmp); // max(dp[j - 1], dp[j])
+        }
+        return res;
     }
+
+    public int lengthOfLongestSubstring1(String s) {
+        Map<Character, Integer> dic = new HashMap<>();
+        int res = 0, tmp = 0;
+        for(int j = 0; j < s.length(); j++) {
+            int i = j - 1;
+            while(i >= 0 && s.charAt(i) != s.charAt(j)) i--; // 线性查找 i
+            tmp = tmp < j - i ? tmp + 1 : j - i; // dp[j - 1] -> dp[j]
+            res = Math.max(res, tmp); // max(dp[j - 1], dp[j])
+        }
+        return res;
+    }
+
+    public int lengthOfLongestSubstring2(String s) {
+        Map<Character, Integer> dic = new HashMap<>();
+        int i = -1, res = 0;
+        for(int j = 0; j < s.length(); j++) {
+            if(dic.containsKey(s.charAt(j)))
+                i = Math.max(i, dic.get(s.charAt(j))); // 更新左指针 i
+            dic.put(s.charAt(j), j); // 哈希表记录
+            res = Math.max(res, j - i); // 更新结果
+        }
+        return res;
+    }
+
 
 
     /**剑指 Offer 19. 正则表达式匹配
