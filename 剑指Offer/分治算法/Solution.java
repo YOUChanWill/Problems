f51f406ac42ab3b36cc55bd2c2681065dff4333a
CHAN W1LL <303661031@qq.com>
2023-09-06 16:12:59 +0800

    剑指Offer：分治算法

--- 剑指Offer/分治算法/Solution.java
+++ 剑指Offer/分治算法/Solution.java
@@ -3,6 +3,7 @@ package 分治算法;
 import DataStructures.HasCycle;
 
 import java.util.HashMap;
+import java.util.Stack;
 
 public class Solution {
 
@@ -63,7 +64,16 @@ public class Solution {
      输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历结果。
      如果是则返回 true，否则返回 false。假设输入的数组的任意两个数字都互不相同。*/
     public boolean verifyPostorder(int[] postorder) {
-
+        Stack<Integer> stack = new Stack<>();
+        int root = Integer.MAX_VALUE;
+        for (int i = postorder.length - 1; i >= 0; i--) {
+            if (postorder[i] > root) return false;
+            while (!stack.isEmpty() && stack.peek() > postorder[i]){
+                root = stack.pop();
+            }
+            stack.add(postorder[i]);
+        }
+        return true;
     }
 
 
