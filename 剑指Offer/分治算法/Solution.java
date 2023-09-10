b9f1b5da844b19fd4672753416a0191db1520651
CHAN W1LL <303661031@qq.com>
2023-09-10 12:07:09 +0800

    剑指Offer：分治算法

--- 剑指Offer/分治算法/Solution.java
+++ 剑指Offer/分治算法/Solution.java
@@ -150,12 +150,17 @@ public class Solution {
     }
 
 
-
     /**剑指 Offer 51. 数组中的逆序对
 
      在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。输入一个数组，求出这个数组中的逆序对的总数。*/
     public int reversePairs(int[] nums) {
-
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (nums[i] > nums[j]) count++;
+            }
+        }
+        return count;
     }
 
 }
