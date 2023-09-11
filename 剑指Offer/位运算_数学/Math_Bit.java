791364bf64f2c4588630b53bba89fabcef557fd9
CHAN W1LL <303661031@qq.com>
2023-09-11 14:12:11 +0800

    剑指Offer：位运算、数学

--- 剑指Offer/位运算_数学/Math_Bit.java
+++ 剑指Offer/位运算_数学/Math_Bit.java
@@ -114,7 +114,13 @@ public class Math_Bit {
 
      数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。你可以假设数组是非空的，并且给定的数组总是存在多数元素。*/
     public int majorityElement(int[] nums) {
-
+        int ans = nums[0], count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (count == 0) ans = nums[i];
+            if (nums[i] == ans) count++;
+            else count--;
+        }
+        return ans;
     }
 
 
