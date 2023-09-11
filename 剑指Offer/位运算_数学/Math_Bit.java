2ac2a264b911c507680547ba8ab627d18fd00eee
CHAN W1LL <303661031@qq.com>
2023-09-11 14:17:20 +0800

    剑指Offer：位运算、数学

--- 剑指Offer/位运算_数学/Math_Bit.java
+++ 剑指Offer/位运算_数学/Math_Bit.java
@@ -1,7 +1,9 @@
 package 位运算_数学;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 
 public class Math_Bit {
 
@@ -161,7 +163,33 @@ public class Math_Bit {
 
      序列内的数字由小到大排列，不同序列按照首个数字从小到大排列。*/
     public int[][] findContinuousSequence(int target) {
+        int i = 1; // 滑动窗口的左边界
+        int j = 1; // 滑动窗口的右边界
+        int sum = 0; // 滑动窗口中数字的和
+        List<int[]> res = new ArrayList<>();
 
+        while (i <= target / 2) {
+            if (sum < target) {
+                // 右边界向右移动
+                sum += j;
+                j++;
+            } else if (sum > target) {
+                // 左边界向右移动
+                sum -= i;
+                i++;
+            } else {
+                // 记录结果
+                int[] arr = new int[j-i];
+                for (int k = i; k < j; k++) {
+                    arr[k-i] = k;
+                }
+                res.add(arr);
+                // 左边界向右移动
+                sum -= i;
+                i++;
+            }
+        }
+        return res.toArray(new int[res.size()][]);
     }
 
 
