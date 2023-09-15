022b87f50e713c24204b33a4a144275001403695
CHAN W1LL <303661031@qq.com>
2023-09-15 20:38:12 +0800

    剑指Offer：位运算、数学

--- 剑指Offer/位运算_数学/Math_Bit.java
+++ 剑指Offer/位运算_数学/Math_Bit.java
@@ -169,7 +169,16 @@ public class Math_Bit {
 
      答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。*/
     public int cuttingRope1(int n) {
-
+        if(n <= 3) return n - 1;
+        int b = n % 3, p = 1000000007;
+        long rem = 1, x = 3;
+        for(int a = n / 3 - 1; a > 0; a /= 2) {
+            if(a % 2 == 1) rem = (rem * x) % p;
+            x = (x * x) % p;
+        }
+        if(b == 0) return (int)(rem * 3 % p);
+        if(b == 1) return (int)(rem * 4 % p);
+        return (int)(rem * 6 % p);
     }
 
 
